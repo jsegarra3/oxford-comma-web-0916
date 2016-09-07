34ce6a6cc7d6efe9bacd0364fa82b59fb0943cbb
James Segarra <jamessegarra3@gmail.com>
2016-09-07 20:52:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  retString = ""
+  if array.length == 1
+    retString = array.join
+  elsif array.length == 2
+    retString = array.join(" and ")
+  elsif array.length > 2
+    lastString = array.pop
+    retString = array.join(", ")
+    retString += ", and #{lastString}"
+  end
+  retString
 end
